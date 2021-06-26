9feccf947629f0d69e4c987074628e03f4d222f9
Yasoob Rasheed <yasoob.f.rasheed@gmail.com>
2021-06-26 13:53:15 -0500

    :tada: initial commit

---
+++ src/components/LazyLoader.js
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function LazyLoader() {
+  return (
+    <div>
+        <h1>
+            This is a Lazy Loader component
+        </h1>
+    </div>
+  );
+}
+
+export default LazyLoader;
